6fbabd133a5522eec122a4049c6335774f5c04f7
Andrei Nigmatulin <andrei.nigmatulin@gmail.com>
2019-08-14 18:12:36 +0100

    [RND-117]: added test

--- jpeg/testdata_test.go
+++ jpeg/testdata_test.go
@@ -34,6 +34,7 @@
 package jpeg_test
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	"image/color"
@@ -105,4 +106,13 @@ Loop:
 			}
 		}
 	}
+}
+
+func TestSampVFactor4Crash(t *testing.T) {
+	data := []byte("\xff\xd8\xff\xdb\x00C\x000000000000000" +
+		"00000000000000000000" +
+		"00000000000000000000" +
+		"00000000000\xff\xc9\x00\v\b00\x000" +
+		"\x01\x01\x14\x00\xff\xda\x00\b\x01\x010\x00?\x0000")
+	jpeg.Decode(bytes.NewReader(data), &jpeg.DecoderOptions{})
 }
